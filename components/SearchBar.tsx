814642ad9715b32a5ff51be3f4a7759221a9ca5d
Bhinsar <bhinsarjagdish2003@gmail.com>
2025-12-03 09:48:21 +0530

    feat: Add initial movie search functionality including UI component and API integration.

---
+++ components/SearchBar.tsx
@@ -0,0 +1,32 @@
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, TextInput, View } from "react-native";
+
+const SearchBar = ({
+  onPress,
+  placeholder,
+}: {
+  onPress?: () => void;
+  placeholder: string;
+}) => {
+  return (
+    <View className="flex-row items-center justify-between bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        placeholderTextColor="#ab8bff"
+        className="flex-1 ml-2 text-white"
+        value=""
+        onChangeText={() => {}}
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
