aa63ee7ca135e45853bfcc98f466ced91c6d0d33
Bhinsar <bhinsarjagdish2003@gmail.com>
2025-12-03 23:15:38 +0530

    feat: Add movie search screen, implement search bar component, and update app layout and dependencies.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,98 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import useFetch from "@/hooks/useFetch";
+import { fetchMovies } from "@/services/api";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 
 const Search = () => {
-  return (
-    <View>
-      <Text>Search</Text>
-    </View>
-  )
-}
+  const [searchQuery, setSearchQuery] = useState("");
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+    refetch,
+  } = useFetch(() => fetchMovies({ query: searchQuery }), false);
 
-export default Search
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (searchQuery.trim()) {
+        refetch();
+      }
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [searchQuery]);
+
+  return (
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 w-full absolute z-0"
+        resizeMode="cover"
+      />
+      <FlatList
+        data={movies}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        className="px-5"
+        contentContainerStyle={{ paddingBlockStart: 100 }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+            <View className="my-5">
+              <SearchBar
+                placeholder="Search movies, series, etc."
+                value={searchQuery}
+                onChangeText={(text: string) => setSearchQuery(text)}
+              />
+            </View>
+            {!moviesLoading &&
+              !moviesError &&
+              searchQuery.trim() &&
+              movies?.length > 0 && (
+                <Text className="text-white text-xl font-bold">
+                  Search Results for{" "}
+                  <Text className="text-accent">{searchQuery}</Text>
+                </Text>
+              )}
+          </>
+        }
+        ListFooterComponent={
+          <>
+            {moviesLoading ? (
+              <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                className="mt-10 self-center"
+              />
+            ) : moviesError ? (
+              <Text className="text-white text-center mt-10">
+                Something went wrong {moviesError}
+              </Text>
+            ) : (
+              movies?.length === 0 && (
+                <View className="mt-10 px-5">
+                  <Text className="text-gray-500 text-center">
+                    No results found for {searchQuery}
+                  </Text>
+                </View>
+              )
+            )}
+          </>
+        }
+      />
+    </View>
+  );
+};
+
+export default Search;
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,16 +1,24 @@
 import { icons } from "@/constants/icons";
 import React from "react";
-import { Image, TextInput, View } from "react-native";
+import { Image, TextInput, TouchableOpacity } from "react-native";
 
 const SearchBar = ({
   onPress,
   placeholder,
+  value,
+  onChangeText,
 }: {
   onPress?: () => void;
   placeholder: string;
+  value?: string;
+  onChangeText?: (text: string) => void;
 }) => {
   return (
-    <View className="flex-row items-center justify-between bg-dark-200 rounded-full px-5 py-2">
+    <TouchableOpacity
+      onPress={onPress}
+      activeOpacity={onPress ? 0.7 : 1}
+      className="flex-row items-center justify-between bg-dark-200 rounded-full px-5 py-2"
+    >
       <Image
         source={icons.search}
         className="size-5"
@@ -18,14 +26,14 @@ const SearchBar = ({
         tintColor="#ab8bff"
       />
       <TextInput
-        onPress={onPress}
         placeholder={placeholder}
         placeholderTextColor="#ab8bff"
         className="flex-1 ml-2 text-white"
-        value=""
-        onChangeText={() => {}}
+        value={value}
+        onChangeText={onChangeText}
+        editable={!onPress}
       />
-    </View>
+    </TouchableOpacity>
   );
 };
 
